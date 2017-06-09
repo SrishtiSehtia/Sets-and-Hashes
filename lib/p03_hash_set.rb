205f9fd00861b6ef97e49987fee8128bb1b16918
App Academy Student <student@appacademy.io>
2017-06-09 15:46:46 -0700

    Completes part 4 linked list

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,29 @@ class HashSet
   end
 
   def insert(key)
+    unless include?(key)
+      @count += 1
+      resize! if @count > num_buckets
+      self[key] << key
+    end
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    @store[key.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +39,10 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.flatten.each do |el|
+      new_store[el % num_buckets * 2] << el
+    end
+    @store = new_store
   end
 end
--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -15,11 +15,26 @@ class Link
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    pr = self.prev
+    nx = self.next
+
+    pr.next = nx
+    nx.prev = pr
+
+    self.next = nil
+    self.prev = nil
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -27,35 +42,76 @@ class LinkedList
     nil
   end
 
+  # first link after the HEAD sentinal
   def first
+    @head.next
   end
 
+  # first link before the TAIL sentinal
   def last
+    @tail.prev
   end
 
   def empty?
+    count = 0
+    node = @head.next
+    until node.val.nil? && node.key.nil?
+      node = node.next
+      count += 1
+    end
+    count == 0
   end
 
   def get(key)
+    link = get_link(key)
+    if link.nil?
+      nil
+    else
+      link.val
+    end
+  end
+
+  def get_link(key)
+    self.each do |link|
+      return link if link.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.any? {|el| el.key == key}
   end
 
   def append(key, val)
+    link = Link.new(key, val)
+
+    link.next = @tail
+    link.prev = @tail.prev
+
+    link.prev.next = link
+    @tail.prev = link
   end
 
   def update(key, val)
+    self.each do |link|
+      link.val = val if link.key == key
+    end
   end
 
   def remove(key)
+    self.get_link(key).remove
   end
 
   def each
+    link = @head.next
+    until link.val.nil? && link.key.nil?
+      yield link
+      link = link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
