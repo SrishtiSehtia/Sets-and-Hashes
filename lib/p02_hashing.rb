e588208a35fecf8540252d60ba0a36a18d63c395
App Academy Student <student@appacademy.io>
2017-06-09 14:00:02 -0700

    Completed hashing

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,14 +1,27 @@
+require 'byebug'
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    return 0 if self.empty?
+    hashes = []
+    self.each_with_index do |el, i|
+      hashes << el.hash + i.hash
+    end
+    hashes.reduce(:^)
   end
 end
 
 class String
   def hash
+    if self.length > 1
+      self.chars.hash
+    else
+      self.ord.hash + self.ord
+    end
   end
 end
 
@@ -16,6 +29,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.sort.hash
   end
 end
